69104df9da0dca90986bf2ed984a05978fde2cf4
Oliver Blanthorn <freedom4cows@gmail.com>
2026-04-22 21:52:42 +0200

    Fix a few missing GB stations

--- make_station_list/gb/wrangler.sql
+++ make_station_list/gb/wrangler.sql
@@ -4,11 +4,11 @@ copy (
     longitude lon,
     latitude lat 
     from (
-        select concat('https://www.realtimetrains.co.uk/search/simple/gb-nr:', d."3ALPHA") as url, concat('70', d.UIC) as uic from (
+        select d."3ALPHA" a3, concat('https://www.realtimetrains.co.uk/search/simple/gb-nr:', d."3ALPHA") as url, concat('70', d.UIC) as uic from (
             select unnest(TIPLOCDATA) d from read_json('CORPUSExtract.json.gz')
         )
         where d.UIC not like '% %' and d."3ALPHA" not like '% %'
     ) c
-    left join '../stations.csv' s on s.uic = c.uic
+    left join '../stations.csv' s on (s.uic = c.uic) or (s.sncf_id = concat('GB', c.a3))
     where latitude is not null and longitude is not null
 ) to 'gb-stations.csv' (header false);
