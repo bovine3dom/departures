0baab74c060da9f7e55f009cab54be51afee4280
Oliver Blanthorn <freedom4cows@gmail.com>
2026-06-05 19:52:30 +0200

    Fix #16: add switzerland

---
+++ make_station_list/ch/wrangler.sql
@@ -0,0 +1,18 @@
+-- clickhouse-local (duckdb gets sad)
+-- select * from '554d4d8a-3720-453d-8571-727e0a0a7097.csv' limit 1;
+
+-- [cn]-stations.csv: country, name, url, lon, lat
+select isoCountryCode country, 
+designationOfficial name,
+concat('https://mesdeparts.ch/?station=', uic) as url,
+wgs84East lon, wgs84North lat,
+number uic
+from '554d4d8a-3720-453d-8571-727e0a0a7097.csv'
+into outfile 'ch-stations.csv' truncate
+-- https://pantallas-estaciones.vercel.app/~/?station=71801
+-- https://mesdeparts.ch/classic/?station=Parent8505307
+
+-- annoyingly trainline is missing lots of UIC for stations
+
+-- https://data.opentransportdata.swiss/dataset/service-point-v2/resource/554d4d8a-3720-453d-8571-727e0a0a7097
+-- can filter for TRAIN and 85
